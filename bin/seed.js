bd7d9886b11f2fa0d1aaa6bd6c9ee4465a1381b5
Blake Ferm <blakeferm@gmail.com>
2020-11-16 14:57:18 -0500

    update repo

--- bin/seed.js
+++ bin/seed.js
@@ -1,38 +1,30 @@
-#!/usr/bin/env node
-
-const {db, Todo} = require('../server/db')
+const { db, Todo } = require("../server/db");
 
 const seed = async () => {
-  await db.sync({force: true})
+  await db.sync({ force: true });
 
   await Todo.create({
-    taskName: 'Buy dog food',
-    assignee: 'Cody'
-  })
+    taskName: "Buy dog food",
+    assignee: "Cody",
+  });
 
   await Todo.create({
-    taskName: 'Take over world',
-    assignee: 'Cody'
-  })
+    taskName: "Take over world",
+    assignee: "Cody",
+  });
 
-  db.close()
+  db.close();
   console.log(`
-
     Seeding successful!
     Time to do stuff!
+  `);
+};
 
-  `)
-}
-
-seed().catch(err => {
-  db.close()
+seed().catch((err) => {
+  db.close();
   console.log(`
-
     Error seeding:
-
     ${err.message}
-
     ${err.stack}
-
-  `)
-})
+  `);
+});
